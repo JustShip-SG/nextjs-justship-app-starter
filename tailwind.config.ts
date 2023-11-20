8a69acf3f200b7c2aef8c7364699dd905e7f8021
shipingl <shiping@justship.sg>
2023-11-20 16:37:34 +0800

    add starter code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,29 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "dark-purple": "#6211CB",
+        "light-purple": "#844FFA",
+      },
+      animation: {
+        sidebounce: "sidebounce 0.7s infinite",
+      },
+      keyframes: {
+        pulse: {
+          "0, 100%": { opacity: "1" },
+          "50%": { opacity: ".7" },
+        },
+        sidebounce: {
+          "0%, 100%": {
+            transform: "translateX(16%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateX(-3%)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
